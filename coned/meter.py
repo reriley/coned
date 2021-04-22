13dfdbda17ce9c2ed66858d65832b56d1f4d08fb
rr.serv@pm.me <rr.serv@pm.me>
2021-04-21 20:00:04 -0400

    Uncommented screenshots

--- coned/meter.py
+++ coned/meter.py
@@ -129,7 +129,7 @@ class Meter(object):
         sleep = 8000
         _LOGGER.debug("Waiting for = %s millis", sleep)
         await page.waitFor(sleep)
-        # await page.screenshot({'path': 'meter0.png'})
+        await page.screenshot({'path': 'meter0.png'})
 
         await page.type("#form-login-email", self.email)
         await page.type("#form-login-password", self.password)
@@ -139,7 +139,7 @@ class Meter(object):
         sleep = 30000
         _LOGGER.debug("Waiting for = %s millis", sleep)
         await page.waitFor(sleep)
-        # await page.screenshot({'path': 'meter1.png'})
+        await page.screenshot({'path': 'meter1.png'})
 
         # Enter in 2 factor auth code (see README for details)
         mfa_code = self.mfa_secret
@@ -147,20 +147,20 @@ class Meter(object):
             mfa_code = pyotp.TOTP(self.mfa_secret).now()
         #_LOGGER.debug("mfa_code = %s", mfa_code)
         await page.type("#form-login-mfa-code", mfa_code)
-        # await page.screenshot({'path': 'meter2.png'})
+        await page.screenshot({'path': 'meter2.png'})
         await page.click(".js-login-new-device-form .button")
         # Wait for authentication to complete
         # await page.waitForNavigation()
         sleep = 30000
         _LOGGER.debug("Waiting for = %s millis", sleep)
         await page.waitFor(sleep)
-        # await page.screenshot({'path': 'meter3.png'})
+        await page.screenshot({'path': 'meter3.png'})
 
         # Access the API using your newly acquired authentication cookies!
         api_page = await browser.newPage()
         api_url = 'https://' + self.data_site + '.opower.com/ei/edge/apis/cws-real-time-ami-v1/cws/' + self.data_site + '/accounts/' + self.account_uuid + '/meters/' + self.meter_number + '/usage'
         await api_page.goto(api_url)
-        # await api_page.screenshot({'path': 'meter4.png'})
+        await api_page.screenshot({'path': 'meter4.png'})
 
         data_elem = await api_page.querySelector('pre')
         self.raw_data = await api_page.evaluate('(el) => el.textContent', data_elem)
